63cc6cd0d3ddcb5f89f1bde1c9936e3788c105ce
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2019-06-27 04:50:26 -0700

    add inst/examples

---
+++ inst/examples/02_guess_correlation/app.R
@@ -0,0 +1,116 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(rexplorables)
+
+generate_data = function(difficulty, numPoints){
+  value = rnorm(1,0,10)
+  valueB = rnorm(1,4,1)
+  outlier = as.numeric( sample(2:6, 1)) #create a random number
+  if (difficulty ==2){ # with outlier
+    choice = sample(2,1)
+    if (choice ==2){
+      X1 = rnorm(numPoints,value,valueB)
+      Y1 = rnorm(numPoints,rnorm(1)*X1,rgamma(1,1)*valueB)
+      mux = mean(X1)
+      sdx = sd(X1)
+      outx = mux + (outlier*sdx) # function for outlier
+      X = c(X1,outx)
+      muy = mean(Y1)
+      sdy = sd(Y1)
+      outy = muy + (outlier*sdy)
+      Y = c(Y1, outy)
+      return(data.frame(X,Y))
+
+    }
+    else{
+      X1 = rnorm(numPoints,value,valueB)
+      Y1 = rnorm(numPoints,rnorm(1)*X1,rgamma(1,1)*valueB)
+      mux = mean(X1)
+      sdx = sd(X1)
+      outx = mux - (outlier*sdx)
+      X = c(X1,outx)
+      muy = mean(Y1)
+      sdy = sd(Y1)
+      outy = muy - (outlier*sdy)
+      Y = c(Y1, outy)
+      return(data.frame(X,Y))
+
+
+    }
+  }
+  else if (difficulty == 1){
+    X = rnorm(numPoints,value,valueB)
+    Y = rnorm(numPoints,rnorm(1)*X,rgamma(1,1)*valueB)
+    return(data.frame(X,Y))
+  }
+
+}
+
+check_exercise <- function(data, guess){
+  actual <- cor(data$X, data$Y)
+  resp <- function(msg, success){
+    message <- if (success){
+     paste(msg, 'The true correlation was', signif(actual, 2))
+    } else {
+      msg
+    }
+    list(message, success)
+  }
+  sgn_incorrect <- sign(actual) != sign(guess)
+  gap = abs(actual - guess)
+  r <- dplyr::case_when(
+    sgn_incorrect ~ resp('Check the sign!', FALSE),
+    gap < 0.1 ~ resp('Awesome!', TRUE),
+    gap < 0.2 ~ resp('Pretty Close! You can do it.', FALSE),
+    TRUE ~ resp("Try again! You can get closer!", FALSE)
+  )
+  names(r) <- c('message', 'success')
+  return(r)
+}
+
+data <- generate_data(1, 50)
+
+sidebar <- function(){
+  tagList(
+    sliderInput(
+      inputId = 'guess',
+      label = 'The correlation is ...',
+      value = 0.5,
+      min = -1,
+      max = 1,
+      step = 0.01
+    )
+  )
+}
+
+
+main <- function(){
+  tagList(
+    plotOutput('plot')
+  )
+}
+
+ui <- navbarPage(
+  title = "Guess the Correlation!",
+  theme = shinythemes::shinytheme('cosmo'),
+  tabPanel("Home",
+    sidebarLayout(
+      sidebarPanel(sidebar(), non_coding_ui('feedback')),
+      mainPanel(main())
+    )
+  )
+)
+
+server <- function(input, output, session){
+  output$plot <- renderPlot({
+    ggplot(data = data, aes(x = X, y = Y)) +
+      geom_point()
+  })
+  feedback <- reactive({
+    check_exercise(data, input$guess)
+  })
+  shiny::callModule(non_coding, 'feedback', feedback)
+}
+
+shinyApp(ui = ui, server = server)
---
+++ inst/examples/03_ab_test_sample_size/app.R
@@ -0,0 +1,102 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(rexplorables)
+
+#' Copied over from https://juliasilge.shinyapps.io/power-app/
+generate_data <- function(baseline, power = 80, signif_level = 5){
+  seq(1000, 1e4, by = 100) %>%
+    purrr::map_df(~power.prop.test(
+      p1 = baseline / 100,
+      p2 = NULL,
+      n = .x,
+      power = power / 100,
+      sig.level = signif_level / 100
+    ) %>%
+      broom::tidy()) %>%
+    mutate(effect = (p2 / p1 - 1))
+}
+
+check_exercise <- function(actual, guess){
+  resp <- function(msg, success){
+    message <- if (success){
+      paste(msg, 'The sample size required is ', actual)
+    } else {
+      msg
+    }
+    list(message, success)
+  }
+  r <- case_when(
+    guess == actual ~ resp("Awesome!", TRUE),
+    TRUE ~ resp("Try Again! You are close", FALSE)
+  )
+  names(r) <- c('message', 'success')
+  r
+}
+
+sidebar <- function() {
+  tagList(
+    sliderInput("sample_size", "Sample Size",
+      min = 1000, max = 10000,
+      value = 1000, step = 500,
+    ),
+    sliderInput("baseline", "Baseline conversion rate",
+      min = 1, max = 50,
+      value = 5, post = "%"
+    ),
+    sliderInput("power", "Power threshold",
+      min = 1, max = 99,
+      value = 80, post = "%"
+    ),
+    sliderInput("signif_level", "Significance level",
+      min = 1, max = 20,
+      value = 5, post = "%"
+    )
+  )
+}
+
+main <- function(){
+  tagList(
+    plotOutput('plot'),
+    tags$hr(),
+    uiOutput("response")
+  )
+}
+
+ui <- navbarPage(
+  title = "Sample Size Calculator",
+  theme = shinythemes::shinytheme('cosmo'),
+  tabPanel("Home",
+    sidebarLayout(
+      sidebarPanel(sidebar(), non_coding_ui('feedback')),
+      mainPanel(main())
+    )
+  )
+)
+
+server <- function(input, output, session){
+  data <- reactive({
+    generate_data(input$baseline)
+  })
+  output$plot <- renderPlot({
+    ggplot(data = data(), aes(x = n, y = effect)) +
+      geom_line() +
+      scale_y_continuous(
+        labels = scales::percent_format(accuracy = 1)) +
+      labs(
+        title = 'MDE vs. Sample Size',
+        x = 'Sample Size',
+        y = 'Minimum Detectable Effect'
+      ) +
+      geom_vline(
+        xintercept = input$sample_size,
+        linetype = 'dotted'
+      )
+  })
+  feedback <- reactive({
+    check_exercise(4000, input$sample_size)
+  })
+  callModule(non_coding, 'feedback', feedback)
+}
+
+shinyApp(ui = ui, server = server)
